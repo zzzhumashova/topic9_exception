bd1cfdfc025340c84ac4db330c442b97e8e5143a
zhumashova.aizhan <102953096+zzzhumashova@users.noreply.github.com>
2023-10-03 10:01:21 +0500

    Add files via upload

---
+++ lesson9.dart
@@ -0,0 +1,53 @@
+//  Create CustomException to check:
+// - age is more than 18, otherwise return “you are teenager yet”
+// void main() {
+//   try {
+//     int age = 14;
+//     checkAge(age);
+//     print("Age checked");
+//   } catch (e) {
+//     print("error $e");
+//   }
+// }
+// void checkAge(int age) {
+//   if (age <= 18) {
+//     print("You are a teenager yet");
+//   }
+//  }
+
+
+// correct phone number
+// void main() {
+//   try {
+//     int number = 87082211238;
+//     number_check(number);
+//   } catch (e) {
+//     print("invalid phone number $e");
+//   }
+// }
+// void number_check(int num) {
+//     if (num == 87082211238) {
+//       print('Correct phone Number');
+//     } else {
+//       print('Invalid phone number');
+//     }
+//   }
+
+
+// correct emai
+//   void main() {
+//   try {
+//     String email = "jihc.kz";
+//     check_email(email);
+//   } catch (e) {
+//     print("Invalid email $e");
+//   }
+// }
+
+// void check_email(String email) {
+//   if (email == "jihc.kz") {
+//     print('Correct email');
+//   } else {
+//     print('Invalid email');
+//   }
+// }
